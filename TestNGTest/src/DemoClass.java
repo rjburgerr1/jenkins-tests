d0e82171ddd8d137957adeac24b1f45c64338903
Randall (RJ) Bradach <49658348+rjburgerr1@users.noreply.github.com>
2021-07-01 12:30:15 -0500

    Update Project

---
+++ TestNGTest/src/DemoClass.java
@@ -0,0 +1,13 @@
+
+public class DemoClass {
+
+	public boolean isInteger(double num) {
+		if (Math.floor(num) == num) {
+			return true ; 
+		} else {
+			return false;
+		}
+			
+	
+	}
+}
--- TestNGTest/src/test1.java
+++ TestNGTest/src/test1.java
@@ -1,9 +1,37 @@
 import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 public class test1 {
+	DemoClass demoClass = new DemoClass();
 
-	@Test
+	@Test(groups = {"tests"})
 	public void test() {
 		System.out.println("HRHRH");
 	}
+	
+	@Test(groups = {"tests", "positive"})
+	public void positiveTest() {
+		boolean result = demoClass.isInteger(12);
+		AssertJUnit.assertTrue(result);
+	}
+	
+	@Test(groups = {"tests", "positive"})
+	public void positiveTest2() {
+		boolean result = demoClass.isInteger(0);
+		AssertJUnit.assertTrue(result);
+	}
+	
+	@Test(groups = {"tests", "negative"})
+	public void negativeTest() {
+		boolean result = demoClass.isInteger(12.232);
+		AssertJUnit.assertFalse(result);
+	}
+	
+	@Test(groups = {"tests", "negative"})
+	public void negativeTest2() {
+		boolean result = demoClass.isInteger(-0.1);
+		AssertJUnit.assertFalse(result);
+	}
+	
 }
-//
